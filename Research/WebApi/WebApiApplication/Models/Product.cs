80f89614de37ab5e6be6153325d3b6d1a2379de3
Jacky Fei <ffflyyy470@outlook.com>
2016-05-11 14:41:04 +0000

    add research project

---
+++ Research/WebApi/WebApiApplication/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace WebApiApplication.Models
+{
+    [DataContract(Name ="product")]
+    public class Product
+    {
+        [Required]
+        [MaxLength(10)]
+        [DataMember(Name = "id", Order = 1)]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Invalid product")]
+        [MaxLength(30)]
+        [DataMember(Name = "name", Order = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.00, 999999.99, ErrorMessage = "Invalid price")]
+        [RegularExpression(@"^\d+\.\d{1,2}$", ErrorMessage = "Invalid price!!")]
+        [DataMember(Name = "price", Order = 3)]
+        public decimal Price { get; set; }
+    }
+}
