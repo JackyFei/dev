6e8ad3b827bb03fc9b00050a91751e79d3e2af42
Jacky Fei <ffflyyy470@hotmail.com>
2016-09-17 20:12:13 +0800

    add web codes

---
+++ FamilyBooks/FamilyBooks.Web/Controllers/ExpenditureController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FamilyBooks.Common.Record;
+
+namespace FamilyBooks.Web.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        [HttpGet]
+        [ActionName("CreateExpenditure")]
+        public ActionResult CreateExpenditure()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("CreateExpenditure")]
+        public ActionResult CreateExpenditure(Expenditure expenditure)
+        {
+            var result = new ContentResult {Content = expenditure.Comment, ContentType = "application/text"};
+            return result;
+        }
+    }
+}
---
+++ FamilyBooks/FamilyBooks.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FamilyBooks.Web.MvcApplication" Language="C#" %>
